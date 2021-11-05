5d0c0bf9b439cb97bad7d2db81a09f508f710301
Victor Queiroz <victorqs1997@hotmail.com>
2021-11-05 09:16:45 -0300

    First Commit

---
+++ app/src/main/java/com/slicedwork/slicedwork/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.slicedwork.slicedwork.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slicedwork.slicedwork.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
